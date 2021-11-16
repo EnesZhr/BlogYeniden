407324f3fa3f1c2659798da5110ef68155b7936b
Enes ZEHİR <enes.zehir@hotmail.com>
2021-11-16 20:45:29 +0300

    Add project files.

---
+++ BlogYeniden/Models/Data/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogYeniden.Models
+{
+    public class User
+    {
+
+        public User()
+        {
+
+        }
+        public User(string username,string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+        public User(int id ,string username, string password)
+        {
+            this.Id = id;
+            this.Username = username;
+            this.Password = password;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
---
+++ BlogYeniden/Models/Data/UygulamaDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogYeniden.Models.Data
+{
+    public class UygulamaDbContext :DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(
+                new User () { Id= 1, Username="admin", Password="123" },
+                new User() { Id = 2, Username = "admin2", Password = "123" }
+                );
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
